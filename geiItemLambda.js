07b3532f435b79292ee354e5c5e1fe9c54ca149e
Victor_leles <victor@incentivar.io>
2023-02-26 22:28:45 -0300

    funções api rest na lambda

---
+++ geiItemLambda.js
@@ -0,0 +1,44 @@
+
+'use strict';
+
+const AWS = require('aws-sdk');
+
+exports.handler = async(event, context) => {
+
+  const documentClient = new AWS.DynamoDB.DocumentClient();
+
+  let responseBody = '';
+  let statusCode = 0;
+
+  const{ id }= event.pathParameters
+
+  const params = {
+    TableName: 'items2',
+    Key:{
+        id:id
+    }
+  }
+
+  try{
+      
+    const data = await documentClient.get(params).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 201
+    
+  }catch(err){
+      
+    responseBody  = `Falha ao inserir iten: ${err}`;
+    statusCode = 403;
+    
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      "content-type": "application/json"
+    },
+    body: responseBody
+  };
+  return response;
+
+}
